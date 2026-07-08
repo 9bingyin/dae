5ebbac83604bab22adf60fdb5ca35d4a4a323f6f
9bingyin <realbingyin@outlook.com>
2026-07-08 19:25:06 +0000

    chore: 配置 nix fmt 使用 treefmt-nix
    - 添加 treefmt-nix flake input
    
    - formatter 输出使用 treefmt，启用 nixfmt/gofmt/shfmt

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
@@ -11,6 +12,7 @@
       self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -22,8 +24,17 @@
             pkgs.llvmPackages_19
           else
             pkgs."llvmPackages_${clangVersion}" or pkgs.llvmPackages_19;
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            gofmt.enable = true;
+            shfmt.enable = true;
+          };
+        };
       in
       {
+        formatter = treefmtEval.config.build.wrapper;
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.go_1_26
